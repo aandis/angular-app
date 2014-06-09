92750baef38e892529ffadf6b0bae8f8bfae774a
aandis <abhishekrm@iitrpr.ac.in>
2014-06-09 23:17:53 +0530

    Added news bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+var news = [
+	
+	"news 1",
+	"news 2",
+	"news 3"
+];
+
 document.addEventListener("DOMContentLoaded", function() 
 {
 	$(".description").hide();
@@ -8,5 +15,14 @@ document.addEventListener("DOMContentLoaded", function()
 	$(".list").mouseleave(function(){
 		$(this).find(".description").hide();
 	});
+
+	var i = 0;
+	setInterval(function(){
+		if(i == 3)
+			i = 0;
+//		$("#news").fadeOut();
+		$("#news").fadeIn().text(news[i]);
+		i = i + 1;
+	},1000);
 
 });
