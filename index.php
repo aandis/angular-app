8d2b3c3019dfe1d063299cf4d97adfdf798b0d9b
aandis <abhishekrm@iitrpr.ac.in>
2014-07-01 19:20:20 +0530

    Temporary theme for now

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
+
     <script type="text/javascript" src = "js/pace.js"></script>
     <script type="text/javascript" src = "js/loadimages.js"></script>
     <script type="text/javascript" src = "js/angular.min.js"></script>
