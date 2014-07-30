3196029363f6fb7c2daff7fda667de72a02c5520
aandis <abhishekrm@iitrpr.ac.in>
2014-07-30 19:55:05 +0530

    Moved animation to animate.css

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
     <link rel="stylesheet" type="text/css" href="css/pace.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
